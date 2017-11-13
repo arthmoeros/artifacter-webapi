163fdf93db9211c3aecd750fe7b0b8f256f1bbdd
Arturo Saavedra <artu.saavedra@gmail.com>
2017-11-12 23:34:31 -0300

    testing dockerfile

--- docker-starter.sh
+++ docker-starter.sh
@@ -1,4 +1,5 @@
-if [ ! -d /etc/qsdt ] then
+if [ ! -d /etc/qsdt ]
+then
     git clone https://github.com/arthmoeros/qsdt-core /usr/src/core-clone
     cp -rf /usr/src/core-clone/config /etc/qsdt
 fi
