45c444799f4a91df0b1bc93baea60c0bbf593e2b
Arturo Saavedra <artu.saavedra@gmail.com>
2017-11-12 23:38:36 -0300

    testing dockerfile

--- docker-starter.sh
+++ docker-starter.sh
@@ -1,6 +1,8 @@
 if [ ! -d /etc/qsdt ]
 then
+    echo "CONFIG NOT SET, setting failover config!"
     git clone https://github.com/arthmoeros/qsdt-core /usr/src/core-clone
     cp -rf /usr/src/core-clone/config /etc/qsdt
 fi
+export QSDT_FS_CONFIG_PATH=/etc/qsdt
 npm start
