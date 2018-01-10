2fea6b79ed4725c235a6c2c33447485d58f6dc9f
arthmoeros <artu.saavedra@gmail.com>
2018-01-10 10:26:17 -0300

    Added option to fetch configuration from git repository

--- docker-starter.sh
+++ docker-starter.sh
@@ -1,8 +1,15 @@
 if [ ! -d $QSDT_CONFIG ]
 then
-    echo "CONFIG NOT SET, setting failover default config!"
-    git clone https://github.com/arthmoeros/qsdt-core /usr/src/core-clone
-    cp -rf /usr/src/core-clone/config $QSDT_CONFIG
+    if [ "$CUSTOM_GIT_CONFIG" = "" ]
+    then
+        echo "CONFIG NOT SET, setting failover default config!"
+        git clone https://github.com/arthmoeros/qsdt-core /usr/src/core-clone
+        cp -rf /usr/src/core-clone/config $QSDT_CONFIG
+    else
+        echo "Custom config variable set, fetching from $CUSTOM_GIT_CONFIG"
+        git clone $CUSTOM_GIT_CONFIG custom_config
+        cp -rf ./custom_config $QSDT_CONFIG
+    fi
 fi
 export QSDT_FS_CONFIG_PATH=$QSDT_CONFIG
 npm start
